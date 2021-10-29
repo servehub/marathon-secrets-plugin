aff2f38a9c3a46fc6253fa1c25ae5bd7596d6930
kulikov <kulikov.dm@gmail.com>
2021-10-29 17:56:34 +0300

    completely new version: generate $SERVICE_PRIVATE_KEY

---
+++ src/main/scala/com/sbuslab/marathon/plugin/MarathonSecretsPlugin.scala
@@ -0,0 +1,132 @@
+package com.sbuslab.marathon.plugin
+
+import java.net.{HttpURLConnection, URL}
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+import java.security.{KeyPairGenerator, SecureRandom, Security}
+
+import javax.crypto.{AEADBadTagException, Cipher, SecretKey, SecretKeyFactory}
+import javax.crypto.spec.{GCMParameterSpec, PBEKeySpec, SecretKeySpec}
+import mesosphere.marathon.plugin.{ApplicationSpec, EnvVarString, PodSpec}
+import mesosphere.marathon.plugin.plugin.PluginConfiguration
+import mesosphere.marathon.plugin.task._
+import net.i2p.crypto.eddsa.{EdDSASecurityProvider, Utils}
+import org.apache.mesos.Protos
+import org.apache.mesos.Protos.{ExecutorInfo, TaskGroupInfo, TaskInfo}
+import play.api.libs.json.{JsObject, Json}
+
+
+class MarathonSecretsPlugin extends RunSpecTaskProcessor with PluginConfiguration {
+
+  private var encryptKey: String = ""
+  private var consulPath: String = ""
+
+  override def initialize(marathonInfo: Map[String, Any], configuration: JsObject): Unit = {
+    encryptKey = (configuration \ "encryptKey").as[String].trim
+    consulPath = (configuration \ "consulPath").as[String].trim
+  }
+
+  Security.addProvider(new EdDSASecurityProvider)
+
+  def taskInfo(appSpec: ApplicationSpec, builder: TaskInfo.Builder): Unit =
+    appSpec.env.get("SERVICE_ID") collect { case serviceId: EnvVarString ⇒
+      require(appSpec.id.toString.startsWith("/" + serviceId.value), throw new Exception("Incorrect $SERVICE_ID env variable!"))
+
+      val resp = (new URL(consulPath + "/private/" + serviceId.value + "?raw=true").openConnection()).asInstanceOf[HttpURLConnection]
+
+      val privateKey =
+        if (resp.getResponseCode == 404) {
+          val generator = KeyPairGenerator.getInstance("EdDSA", "EdDSA")
+          val pair = generator.generateKeyPair()
+
+          consulPut(consulPath + "/public/" + serviceId.value, Json.toJson(Map("publicKey" → Utils.bytesToHex(pair.getPublic.getEncoded))).toString())
+
+          val privateHex = Utils.bytesToHex(pair.getPrivate.getEncoded)
+
+          consulPut(consulPath + "/private/" + serviceId.value, Json.toJson(Map("privateKey" → AesPbkdf2.encrypt(encryptKey, privateHex))).toString())
+
+          privateHex
+        } else {
+          AesPbkdf2.decrypt(encryptKey, (Json.parse(resp.getInputStream) \ "privateKey").as[String])
+        }
+
+      val envBuilder = builder.getCommand.getEnvironment.toBuilder
+
+      envBuilder.addVariables(
+        Protos.Environment.Variable.newBuilder()
+          .setName("SERVICE_PRIVATE_KEY")
+          .setValue(privateKey))
+
+      builder.setCommand(
+        builder.getCommand.toBuilder
+          .setEnvironment(envBuilder))
+    }
+
+  private def consulPut(url: String, body: String): Int = {
+    val conn1 = (new URL(url).openConnection()).asInstanceOf[HttpURLConnection]
+    conn1.setRequestMethod("PUT")
+    conn1.setDoOutput(true)
+
+    val bytes = body.getBytes("utf-8")
+    val os = conn1.getOutputStream
+    os.write(bytes, 0, bytes.length)
+
+    conn1.getResponseCode
+  }
+
+  def taskGroup(podSpec: PodSpec, executor: ExecutorInfo.Builder, taskGroup: TaskGroupInfo.Builder): Unit = {}
+
+
+  object AesPbkdf2 {
+    private val VERSION: Integer = 1
+    private val SALT_SIZE: Int   = 16
+    private val IV_SIZE: Int     = 12
+    private val ITERATIONS: Int  = 200000
+
+    def encrypt(secret: String, text: String): String = try {
+      val salt: Array[Byte] = new Array[Byte](SALT_SIZE)
+      val random: SecureRandom = SecureRandom.getInstanceStrong
+      random.nextBytes(salt)
+
+      val key: SecretKey = new SecretKeySpec(SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256").generateSecret(new PBEKeySpec(secret.toCharArray, salt, ITERATIONS, 256)).getEncoded, "AES")
+      val iv: Array[Byte] = new Array[Byte](IV_SIZE)
+      random.nextBytes(iv)
+
+      val cipher: Cipher = Cipher.getInstance("AES/GCM/NoPadding")
+      cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(128, iv))
+
+      val encrypted: Array[Byte] = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8))
+      val buf: ByteBuffer = ByteBuffer.allocate(1 + SALT_SIZE + IV_SIZE + encrypted.length)
+      buf.put(VERSION.byteValue).put(salt).put(iv).put(encrypted)
+
+      java.util.Base64.getEncoder.encodeToString(buf.array)
+    } catch {
+      case e: Exception ⇒
+        throw new RuntimeException(e)
+    }
+
+    def decrypt(secret: String, data: String): String = try {
+      val encrypted: Array[Byte] = java.util.Base64.getDecoder.decode(data)
+
+      val version: Int = java.util.Arrays.copyOfRange(encrypted, 0, 1)(0)
+      if (version != VERSION) throw new RuntimeException("Unknown encryption version")
+
+      val salt: Array[Byte] = java.util.Arrays.copyOfRange(encrypted, 1, 1 + SALT_SIZE)
+      val iv: Array[Byte] = java.util.Arrays.copyOfRange(encrypted, 1 + SALT_SIZE, 1 + SALT_SIZE + IV_SIZE)
+      val key: SecretKey = new SecretKeySpec(SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256").generateSecret(new PBEKeySpec(secret.toCharArray, salt, ITERATIONS, 256)).getEncoded, "AES")
+
+      val cipher: Cipher = Cipher.getInstance("AES/GCM/NoPadding")
+      cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, iv))
+
+      val decrypted: Array[Byte] = cipher.doFinal(java.util.Arrays.copyOfRange(encrypted, 1 + SALT_SIZE + IV_SIZE, encrypted.length))
+
+      new String(decrypted, StandardCharsets.UTF_8)
+    } catch {
+      case e: AEADBadTagException ⇒
+        throw new RuntimeException("AES: incorrect password", e)
+
+      case e: Exception ⇒
+        throw new RuntimeException(e)
+    }
+  }
+}
---
+++ src/test/scala/com/sbuslab/marathon/plugin/MarathonSecretsPluginTest.scala
@@ -0,0 +1,53 @@
+package com.sbuslab.marathon.plugin
+
+import scala.collection.immutable
+
+import mesosphere.marathon.plugin._
+import org.apache.mesos.Protos.TaskInfo
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import play.api.libs.json.{JsObject, Json}
+
+
+@RunWith(classOf[JUnitRunner])
+class MarathonSecretsPluginTest extends FunSuite {
+
+  private val config = Json.parse("""
+    {
+      "encryptKey": "aiHag0kaimaiSh5hiepheikieGhieKai2ui6uqu0aix9ewie9Laehahb8ohxahsh",
+      "consulPath": "https://localhost:8500/v1/kv/services/keys"
+    }
+  """).as[JsObject]
+
+  val appSpec: ApplicationSpec = new ApplicationSpec {
+    val env: Map[String, EnvVarValue] = Map(
+      "SERVICE_ID" → new EnvVarString { val value = "exchange/order-service-test" },
+    )
+
+    val id: PathId = new PathId {
+      def path: immutable.Seq[String] = List("exchange", "order-service-test")
+      override def toString = "/" + path.mkString("/")
+    }
+
+    val user: Option[String] = Some("root")
+    val labels: Map[String, String] = Map.empty
+    val acceptedResourceRoles: Set[String] = Set.empty
+    val secrets: Map[String, mesosphere.marathon.plugin.Secret] = Map.empty
+    val networks: Seq[mesosphere.marathon.plugin.NetworkSpec] = Nil
+    val volumes: Seq[mesosphere.marathon.plugin.VolumeSpec] = Nil
+    val volumeMounts: Seq[VolumeMountSpec] = Nil
+  }
+
+
+  test("test") {
+    val plugin = new MarathonSecretsPlugin()
+
+    plugin.initialize(Map.empty, config)
+
+    val builder = TaskInfo.newBuilder()
+
+    plugin.taskInfo(appSpec, builder)
+  }
+
+}
